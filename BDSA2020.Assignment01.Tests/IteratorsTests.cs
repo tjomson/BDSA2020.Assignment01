5de75d9901c0776512877f77216344a2d565c482
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-09-05 13:04:11 +0200

    Add working tests for iterators

--- BDSA2020.Assignment01.Tests/IteratorsTests.cs
+++ BDSA2020.Assignment01.Tests/IteratorsTests.cs
@@ -1,15 +1,60 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
+using BDSA2019.Assignment01;
 
 namespace BDSA2020.Assignment01.Tests
 {
     public class IteratorsTests
     {
         [Theory]
-        [InlineData(true)]
-        public void AssertTrue(bool shouldPass)
+        [InlineData(1, 2, 3, 4, 5, 6)]
+        [InlineData(0, -1, 7, 3.98, -200, 29)]
+        public void FlattenTest(int a, int b, int c, int d, int e, int f)
         {
-            Assert.True(shouldPass);
+            var list = new List<List<int>>();
+            var expected = new int[] { a, b, c, d, e, f };
+
+            list.Add(new List<int>(){
+                a,b,c
+            });
+
+            list.Add(new List<int>(){
+                d,e,f
+            });
+
+
+            int counter = 0;
+            foreach (var item in Iterators.Flatten(list))
+            {
+                Assert.Equal(expected[counter], item);
+                counter++;
+            }
+
         }
 
+        [Theory]
+        [InlineData(new int[]{1,2,3,4}, new int[]{2,4})]
+        [InlineData(new int[]{-5,-8,1001,0}, new int[]{-8, 0})]
+        public void FilterTest(int[] input, int[] expected)
+        {
+
+            Predicate<int> even = Even;
+            
+            var actual = Iterators.Filter(input, even);
+
+            var counter = 0;
+            foreach(var item in actual)
+            {
+                Assert.Equal(expected[counter], item);
+                counter++;
+            }
+
+        }
+
+        public static bool Even(int i)
+        {
+            return i % 2 == 0;
+        }
     }
 }
--- BDSA2020.Assignment01/Iterators.cs
+++ BDSA2020.Assignment01/Iterators.cs
@@ -1,27 +1,38 @@
 using System;
 using System.Collections.Generic;
 
-namespace BDSA2020.Assignment01
+namespace BDSA2019.Assignment01
 {
     public static class Iterators
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            foreach(var item in items)
+
+            foreach (var item in items)
             {
-                foreach(var i in item)
+                foreach (var i in item)
                 {
-                    yield return i; //Måske rigtigt
+                    yield return i;
                 }
             }
-            
-            
-            throw new NotImplementedException();
+
+
+
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
         }
+
+    
+
+        
     }
 }
